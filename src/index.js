1d6b5327e82a3a16e6a3786b26ea6e7231149bc3
g-harel <gabrielj.harel@gmail.com>
2017-12-17 15:37:31 -0500

    started building interactive interface

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
 require('./style.scss');
 require('./index.html');
+
+require('okwolo');
+
+const okwolo = window.okwolo;
+
+const app = okwolo(document.querySelector('.wrapper'));
+
+app.setState({});
+
+app(() => () => (
+    ['div', {}, [
+        ['div.banner', {}, [
+            ['div.title', {},
+                'okwolo'.split('').map((letter) => (
+                    ['div.letter', {}, [
+                        ['img', {src: `/res/letters/${letter}.svg`}],
+                        ['img.shadow', {src: `/res/letters/${letter}.svg`}],
+                    ]]
+                )),
+            ],
+            ['div.subtitle', {}, [
+                'light javascript framework to build web applications',
+            ]],
+            ['div.buttons', {}, [
+                ['div.button', {}, [
+                    'Get Started',
+                ]],
+                ['div.button.shadow', {}, [
+                    'Get Started',
+                ]],
+            ]],
+        ]],
+    ]]
+));
+
