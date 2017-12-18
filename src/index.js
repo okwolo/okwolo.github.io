28fc68c1b920e0692977ce5cb35ac472d99e29ff
g-harel <gabrielj.harel@gmail.com>
2017-12-17 19:32:54 -0500

    added tiles section

--- src/index.js
+++ src/index.js
@@ -7,13 +7,33 @@ const okwolo = window.okwolo;
 
 const app = okwolo(document.querySelector('.wrapper'));
 
-app.setState({});
+app.setState({
+    title: 'okwolo',
+    tiles: [
+        {
+            title: 'syntax',
+            copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        },
+        {
+            title: 'blobs',
+            copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in.',
+        },
+        {
+            title: 'modules',
+            copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.',
+        },
+        {
+            title: 'kits',
+            copy: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        },
+    ],
+});
 
-app(() => () => (
+app(() => ({tiles, title}) => (
     ['div', {}, [
         ['div.banner', {}, [
             ['div.title', {},
-                'okwolo'.split('').map((letter) => (
+                title.split('').map((letter) => (
                     ['div.letter', {}, [
                         ['img', {src: `/res/letters/${letter}.svg`}],
                         ['img.shadow', {src: `/res/letters/${letter}.svg`}],
@@ -27,11 +47,27 @@ app(() => () => (
                 ['div.button', {}, [
                     'Get Started',
                 ]],
-                ['div.button.shadow', {}, [
-                    'Get Started',
-                ]],
             ]],
         ]],
+        ['div.tiles', {},
+            tiles.map(({title, copy}) => (
+                ['div.tile', {}, [
+                    ['div.icon', {}, [
+                        ['img', {src: `/res/icons/${title}.svg`}],
+                        ['img.shadow', {src: `/res/icons/${title}.svg`}],
+                    ]],
+                    ['div.title', {}, [
+                        title,
+                    ]],
+                    ['div.copy', {}, [
+                        copy,
+                    ]],
+                    ['div.link', {}, [
+                        'more info',
+                    ]],
+                ]]
+            )),
+        ],
     ]]
 ));
 
